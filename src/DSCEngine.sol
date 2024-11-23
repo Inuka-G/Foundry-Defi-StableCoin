0a9f1a2900373200ecb1926d7a267e0e47b232c5
Inuka-G <156221307+Inuka-G@users.noreply.github.com>
2024-11-23 23:12:06 +0530

    Update depositCollateral
    Update depositCollateral

--- src/DSCEngine.sol
+++ src/DSCEngine.sol
@@ -1,6 +1,14 @@
 // SPDX-License-Identifier: SEE LICENSE IN LICENSE
 pragma solidity ^0.8.18;
 
+import {DecentralizedStableCoin} from "./DecentralizedStableCoin.sol";
+import {ReentrancyGuard} from "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+///////////////////////////////////////////////////
+// Developed by Axion chain labs  //////////////////
+///////////////////////////////////////////////////
+
 /**
  * @title DSC Engine contract
  * @author inukaG on behalf of axion chain labs
@@ -16,11 +24,76 @@ pragma solidity ^0.8.18;
  *
  * this contract responsible for minting and burning of stablecoin tokens and deposit and withdraw of collateral
  */
+contract DSCEngine is ReentrancyGuard {
+    ///////////////
+    // Errors    //
+    ///////////////
+    error DSCEngine__InvalidAmount();
+    error DSCEngine__tokenAddressesNotEqualtoPricefeedAddresses();
+    error DSCEngine__NotAllowedToken();
+    ///////////////////////
+    // State variables   //
+    ///////////////////////
 
-contract DSCEngine {
-    function depositCollateralAndMintAXUSD() public {}
+    mapping(address token => address priceFeed) public s_priceFeeds;
+    mapping(address user => mapping(address tokenCollateralAddress => uint256 amount)) public s_collateralDeposited;
 
-    function depositCollateral() public {}
+    DecentralizedStableCoin public immutable i_stableCoinContractAddress;
+
+    ///////////////
+    // Events    //
+    ///////////////
+
+    event CollateralDeposited(address indexed user, address indexed tokenCollateralAddress, uint256 amountColletaral);
+
+    ///////////////
+    // Modifiers //
+    ///////////////
+
+    modifier notZeroAmount(uint256 amount) {
+        if (amount <= 0) {
+            revert DSCEngine__InvalidAmount();
+        }
+        _;
+    }
+
+    modifier isAllowedToken(address tokenContractAddress) {
+        if (s_priceFeeds[tokenContractAddress] == address(0)) {
+            revert DSCEngine__NotAllowedToken();
+        }
+        _;
+    }
+
+    ///////////////
+    // Functions //
+    ///////////////
+    constructor(address[] memory tokenAddresses, address[] memory priceFeedAddresses, address axionStableCoinAddress) {
+        if (tokenAddresses.length != priceFeedAddresses.length) {
+            revert DSCEngine__tokenAddressesNotEqualtoPricefeedAddresses();
+        }
+        for (uint256 i = 0; i < tokenAddresses.length; i++) {
+            s_priceFeeds[tokenAddresses[i]] = priceFeedAddresses[i];
+        }
+        i_stableCoinContractAddress = DecentralizedStableCoin(axionStableCoinAddress);
+    }
+
+    function depositCollateralAndMintAXUSD() external {}
+
+    /**
+     * @param tokenCollateralAddress address of the token to be deposited as collateral (weth or wbtc contract address)
+     * @param amountColletaral amount of token to be deposited as collateral
+     */
+    function depositCollateral(address tokenCollateralAddress, uint256 amountColletaral)
+        external
+        notZeroAmount(amountColletaral)
+        isAllowedToken(tokenCollateralAddress)
+        nonReentrant
+    {
+        // deposit the collateral and add already deposited amount +=
+        s_collateralDeposited[msg.sender][tokenCollateralAddress] += amountColletaral;
+        emit CollateralDeposited(msg.sender, tokenCollateralAddress, amountColletaral);
+        IERC20(tokenCollateralAddress).transferFrom(msg.sender, address(this), amountColletaral);
+    }
 
     function redeemCollateralForAXUSD() public {}
 
